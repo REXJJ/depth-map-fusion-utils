d676d332dc54f9e84789453e42b0f466308843fd
REXJJ <rexjj7@gmail.com>
2020-10-12 14:07:06 -0700

    Up to date

--- src/IdentifySweetSpot.cpp
+++ src/IdentifySweetSpot.cpp
@@ -153,62 +153,72 @@ int main(int argc, char** argv)
     }
 #else
     pcl::PLYReader Reader;
-    Reader.read("/home/rex/REX_WS/Test_WS/POINT_CLOUD_STITCHING/data/empty_box_inside/"+string(argv[1])+".ply", *cloud);
+    Reader.read("/home/rex/REX_WS/Test_WS/POINT_CLOUD_STITCHING/data/reference_cam_calibration_outside_box/"+string(argv[1])+".ply", *cloud);
 #endif
-    for(auto &pt:cloud->points)
-        pt.z*=-1;
-    PointCloud<PointXYZ>::Ptr cloud_bw (new PointCloud<PointXYZ>);
-    for(int i=0;i<cloud->points.size();i++)
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_output (new pcl::PointCloud<pcl::PointXYZRGB>);
+    int counter = 0;
+    for(auto pt:cloud->points)
     {
-        PointXYZ ptxyz;
-        PointXYZRGB ptxyzrgb = cloud->points[i];
-        ptxyz.x=ptxyzrgb.x;
-        ptxyz.y=ptxyzrgb.y;
-        ptxyz.z=ptxyzrgb.z;
-        cloud_bw->points.push_back(ptxyz);
+        if(pt.z*-1>1.0)
+            continue;
+        cloud_output->points.push_back(pt);
+        counter++;
     }
-    pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
-    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
-    ne.setInputCloud (cloud_bw);
-    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
-    ne.setSearchMethod (tree);
-    pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
-    ne.setRadiusSearch (0.005);
-    ne.setViewPoint(0,0,0);
-    ne.compute (*normals); 
-    
-    PointCloud<PointXYZRGB>::Ptr cloud_classified (new PointCloud<PointXYZRGB>);
-
-    for(int i=0;i<normals->points.size();i++)
-    {
-        Normal ptn = normals->points[i];
-        int angle_z = angle(ptn.normal);
-        PointXYZ pt = cloud_bw->points[i];
-        PointXYZRGB pt_new;
-        pt_new.x = pt.x;
-        pt_new.y = pt.y;
-        pt_new.z = pt.z;
-        pt_new.g = 255;
-        pt_new.r = 0;
-        pt_new.b = 0;
-        if(angle_z>=0&&angle_z<=45)
-        {
-            pt_new.b = 0;
-            pt_new.r = 255;
-            pt_new.g = 0;
-        }
-        cloud_classified->points.push_back(pt_new);
-    }
-
-
-    computeNormalAnglesRange(normals);
+    cloud_output->height=1;
+    cloud_output->width = counter;
     VisualizationUtilities::PCLVisualizerWrapper viz;
-    viz.addPointCloudNormals<PointXYZ>(cloud_bw,normals);
-    viz.addPointCloud<PointXYZRGB>(cloud);
+    viz.addPointCloud<PointXYZRGB>(cloud_output);
     PointXYZ ptorigin = PointXYZ(0,0,0);
     viz.addSphere(ptorigin);
     viz.addCoordinateSystem();
     viz.spinViewer();
+    pcl::io::savePCDFileASCII ("cleaned"+string(argv[1])+".pcd", *cloud_output);
+    // PointCloud<PointXYZ>::Ptr cloud_bw (new PointCloud<PointXYZ>);
+    // for(int i=0;i<cloud->points.size();i++)
+    // {
+    //     PointXYZ ptxyz;
+    //     PointXYZRGB ptxyzrgb = cloud->points[i];
+    //     ptxyz.x=ptxyzrgb.x;
+    //     ptxyz.y=ptxyzrgb.y;
+    //     ptxyz.z=ptxyzrgb.z;
+    //     cloud_bw->points.push_back(ptxyz);
+    // }
+    // pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
+    // pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+    // ne.setInputCloud (cloud_bw);
+    // pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
+    // ne.setSearchMethod (tree);
+    // pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+    // ne.setRadiusSearch (0.005);
+    // ne.setViewPoint(0,0,0);
+    // ne.compute (*normals); 
+    //
+    // PointCloud<PointXYZRGB>::Ptr cloud_classified (new PointCloud<PointXYZRGB>);
+    //
+    // for(int i=0;i<normals->points.size();i++)
+    // {
+    //     Normal ptn = normals->points[i];
+    //     int angle_z = angle(ptn.normal);
+    //     PointXYZ pt = cloud_bw->points[i];
+    //     PointXYZRGB pt_new;
+    //     pt_new.x = pt.x;
+    //     pt_new.y = pt.y;
+    //     pt_new.z = pt.z;
+    //     pt_new.g = 255;
+    //     pt_new.r = 0;
+    //     pt_new.b = 0;
+    //     if(angle_z>=0&&angle_z<=45)
+    //     {
+    //         pt_new.b = 0;
+    //         pt_new.r = 255;
+    //         pt_new.g = 0;
+    //     }
+    //     cloud_classified->points.push_back(pt_new);
+    // }
+    //
+    //
+    // computeNormalAnglesRange(normals);
+
     // auto temp = minmax_element(cloud_bw->points.begin(),cloud_bw->points.end(),[](PointXYZ A,PointXYZ B){return A.z<B.z;});
     // cout<<*temp.first<<" "<<*temp.second<<endl;
     return 0;
