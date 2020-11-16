d55073557cc8844a110cb0a8794f4941f473575f
REXJJ <rexjj7@gmail.com>
2020-11-16 12:05:50 -0800

    Added pcl plane fitting.

--- tests/PointCloudFilter.cpp
+++ tests/PointCloudFilter.cpp
@@ -65,52 +65,14 @@ void visualize(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud)
     viz.spinViewer();
 }
 
-Eigen::Vector3f getNormal(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
-{
-  Eigen::MatrixXd lhs (cloud->size(), 3);
-  Eigen::VectorXd rhs (cloud->size());
-  for (size_t i = 0; i < cloud->size(); ++i)
-  {
-    const auto& pt = cloud->points[i];
-    lhs(i, 0) = pt.x;
-    lhs(i, 1) = pt.y;
-    lhs(i, 2) = 1.0;
-
-    rhs(i) = -1.0 * pt.z;
-  }
-  Eigen::Vector3d params = lhs.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(rhs);
-  Eigen::Vector3d normal (params(0), params(1), 1.0);
-  auto length = normal.norm();
-  normal /= length;
-  params(2) /= length;
-  return {normal(0), normal(1), normal(2)};
-}
-
-double euclideanNorm(Vector3f a,Vector3f b)
-{
-    return (a-b).norm();
-}
-
-double pointToVectorDistance(Vector3f pt, Vector3f norm_pt, Vector3f n)
-{
-   Vector3f d_xyz = n*ball_radius*2;
-   Vector3f v1 = norm_pt - d_xyz;
-   Vector3f v2 = norm_pt + d_xyz;
-    Vector3f a = v1 -v2;
-    Vector3f b = pt - v2;
-    if(a.norm()==0.0)
-        return 0;
-    return a.cross(b).norm()/a.norm();
-}
-
 Vector3f projectPointToVector(Vector3f pt, Vector3f norm_pt, Vector3f n)
 {
-    Vector3f d_xyz = n*ball_radius*2;
+    Vector3f d_xyz = n*ball_radius;
     Vector3f a = norm_pt - d_xyz;
     Vector3f b = norm_pt + d_xyz;
     Vector3f ap = (a-pt);
     Vector3f ab = (a-b);
-    Vector3f p = a + ((ap.dot(ab))/ab.norm())*ab;
+    Vector3f p = a - (ap.dot(ab)/ab.dot(ab))*ab;
     return p;
 }
 
@@ -121,8 +83,13 @@ void process(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,pcl::PointCloud<pcl::P
     pcl::copyPointCloud(*cloud,*cloud_bw);
     tree->setInputCloud (cloud_bw);
     unsigned long long int good_centers = 0;
+    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
     for(int i=0;i<centroids->points.size();i++)
     {
+        if(i%100==0)
+        {
+            std::cout<<i<<" out of "<<centroids->points.size()<<" done."<<std::endl;
+        }
         auto pt = centroids->points[i];
         // pcl::PointXYZ ptxyz = pcl::PointXYZ({pt.x,pt.y,pt.z});
         pcl::PointXYZ ptxyz;
@@ -132,24 +99,18 @@ void process(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,pcl::PointCloud<pcl::P
         vector<int> indices;
         vector<float> dist;
         auto t=tree->radiusSearch(ptxyz,ball_radius,indices,dist,0);
-        pcl::PointCloud<pcl::PointXYZ>::Ptr points( new pcl::PointCloud<pcl::PointXYZ> );
-        for(auto ids:indices)
-        {
-            PointXYZ pt_temp;
-            pt_temp.x = cloud->points[ids].x;
-            pt_temp.y = cloud->points[ids].y;
-            pt_temp.z = cloud->points[ids].z;
-            points->points.push_back(pt_temp);
-        }
+        float curvature;
+        Eigen::Vector4f plane_parameters;
+        ne.computePointNormal(*cloud_bw,indices,plane_parameters,curvature);
         int good_points = 0;
-#if 1
         if(indices.size()>10)
         {
-            Vector3f normal = getNormal(points);
+            Vector3f normal = {plane_parameters(0),plane_parameters(1),plane_parameters(2)};
             Vector3f norm_pt;
             norm_pt<<pt.x,pt.y,pt.z;
             Vector3f pt_pro;
             pt_pro<<0,0,0;
+            double weights = 0.0;
             for(auto ids:indices)
             {
                 PointXYZ pt_temp;
@@ -158,11 +119,13 @@ void process(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,pcl::PointCloud<pcl::P
                 pt_temp.z = cloud->points[ids].z;
                 Vector3f pt_loc;
                 pt_loc<<pt_temp.x,pt_temp.y,pt_temp.z;
-                double distance_to_normal = pointToVectorDistance(pt_loc, norm_pt, normal);
+                Vector3f projected_points = projectPointToVector(pt_loc, norm_pt,normal);
+                double distance_to_normal = (pt_loc - projected_points).norm();
+                // std::cout<<"Distance To Normal: "<<distance_to_normal<<" Distance to normal Pt: "<<(pt_loc-norm_pt).norm()<<std::endl;
                 if(distance_to_normal<cylinder_radius)
                 {
-                    Vector3f projected_points = projectPointToVector(pt_loc, norm_pt,normal);
-                    pt_pro+=projected_points;
+                    pt_pro+= (1.0-distance_to_normal/cylinder_radius) * projected_points;
+                    weights += (1.0-distance_to_normal/cylinder_radius);
                     // cout<<"Points: "<<pt_loc<<" Projected Points: "<<projected_points<<" Normal: "<<normal<<endl;
                     good_points++;
 
@@ -173,31 +136,31 @@ void process(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,pcl::PointCloud<pcl::P
                 cout<<"No Good Points.."<<endl;
                 continue;
             }
-            pt_pro/=good_points;
+            pt_pro/=weights;
             PointXYZRGB pt_processed;
             pt_processed.x = pt_pro(0);
             pt_processed.y = pt_pro(1);
             pt_processed.z = pt_pro(2);
-            pt_processed.r = 0;
-            pt_processed.g = 0;
-            pt_processed.b = 0;
+            pt_processed.r = pt.r;
+            pt_processed.g = pt.g;
+            pt_processed.b = pt.b;
             good_centers++;
             processed->points.push_back(pt_processed); 
         }
-#endif
     }
     processed->height = 1;
-    processed->height = good_centers;
+    processed->width = good_centers;
 }
+
 
 int main(int argc, char** argv)
 {
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-#if 0
+#if 1
     if (pcl::io::loadPCDFile<pcl::PointXYZRGB> (argv[1], *cloud) == -1) //* load the file
     {
         PCL_ERROR ("Couldn't read file for base. \n");
-        return;
+        return -1;
     }
 #else
     pcl::PLYReader Reader;
